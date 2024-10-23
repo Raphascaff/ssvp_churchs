37151fc58495acd4296d5bbfbff219fe02bae476
Raphael Scaff <raphael.scaff@vadetaxi.com.br>
2024-10-23 20:17:35 -0300

    Feature(Agrid): Add Agrid to dataFrame

--- Dash/dash.py
+++ Dash/dash.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import plotly.express as px
-from Data.data import df
+from Data.data import df as initial_df
 from Data.get_env_vars import GetEnvVars
 from dataclasses import dataclass
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 
 @dataclass
@@ -16,20 +18,46 @@ class Dash(GetEnvVars):
 
         st.markdown("<h1 style='text-align: center; color: white;'> Igrejas do Brasil </h1>", unsafe_allow_html=True)
 
+        # Use session state to hold the DataFrame
+        if 'df' not in st.session_state:
+            st.session_state.df = initial_df.copy()
+
         tab1, tab2 = st.tabs(["Data", "Map"])
 
         with tab1:
             st.write("### DataFrames")
-            st.data_editor(df)
-            save_button = st.button(label="JSON", type="primary")
-            if save_button:
-                result = df.to_json(force_ascii=False, indent=4)
-                with open('Scrappers/results.json', 'w', encoding='utf-8') as json_file:
-                    json_file.write(result)
-                    st.success("Data was successfully saved!")
+
+            # Configure AgGrid with the DataFrame from session state
+            gb = GridOptionsBuilder.from_dataframe(st.session_state.df)
+            gb.configure_pagination()
+            gridOptions = gb.build()
+
+            # Make AgGrid editable
+            AgGrid(st.session_state.df, gridOptions=gridOptions, editable=True, update_mode='MODEL_CHANGED')
+
+            # Inputs for creating a new column
+            input_text = st.text_input(label="Column Name: ")
+            position = st.number_input(label=f"Position: 0 to {len(st.session_state.df.columns)}:", value=len(st.session_state.df.columns))
+
+            c1, c2 = st.columns([2, 26])
+            with c1:
+                save_button = st.button(label="Save JSON", type="primary")
+                if save_button:
+                    result = st.session_state.df.to_json(force_ascii=False, indent=4)
+                    with open('Scrappers/results.json', 'w', encoding='utf-8') as json_file:
+                        json_file.write(result)
+                        st.success("Data was successfully saved!")
+            with c2:
+                # Enable the button based on the validity of the input
+                create_column_button = st.button(label="Create Column", type="primary", 
+                                                  disabled=not (input_text and position is not None and 0 <= position <= len(st.session_state.df.columns)))
+
+                if create_column_button:
+                    st.session_state.df.insert(int(position), input_text, value="")
+                    st.success(f"Column '{input_text}' added at position {position}!")
 
         with tab2:
-            fig = px.scatter_mapbox(df, 
+            fig = px.scatter_mapbox(st.session_state.df, 
                                     lat='LATITUDE', 
                                     lon='LONGITUDE', 
                                     hover_name='TITLE',
