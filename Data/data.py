9cd523b5f67ef94c1d744a28afb0db95c50233bd
Raphael Scaff <raphael.scaff@vadetaxi.com.br>
2024-10-23 18:51:28 -0300

    Refactor(All): Refact all project structure

--- Data/data.py
+++ Data/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 
-with open('results.json') as json_file:
+with open('Scrappers/results.json') as json_file:
     data = json.load(json_file)
 
 df = pd.json_normalize(data)
